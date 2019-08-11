ba7d7e8b2bee746fd425e8a86bf09751fd6ae71a
Jordan <Gaza>
2019-08-11 00:04:19 -0400

    Continuing work on final display

--- js/script.js
+++ js/script.js
@@ -33,6 +33,6 @@ if (eventType === "casual" || eventType === "semi-formal" || eventType === "form
     }
 }
 
-console.log(result.concat("Since it is" + tempFarh + " and you are going to " + eventType + " you should wear" ))
+console.log(result.concat("Since it is" + tempFarh + " and you are going to " + eventType + " you should wear" + result))
 
 
